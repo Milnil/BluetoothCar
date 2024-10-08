98d32a11ef9c49d6146487d6e293b7a539e5b5a1
PNPTCMilind <m.philar@pnptc.com>
2024-10-08 15:55:05 -0500

    hi

--- pi_socket.py
+++ pi_socket.py
@@ -1,100 +1,58 @@
 import socket
 import threading
-from collections import deque
 import signal
 import time
 
-server_addr = 'D8:3A:DD:F8:70:E3'
 server_port = 1
-
 buf_size = 1024
-
 client_sock = None
 server_sock = None
-sock = None
-
 exit_event = threading.Event()
-
-message_queue = deque([])
-output = ""
-
-dq_lock = threading.Lock()
-output_lock = threading.Lock()
 
 def handler(signum, frame):
     exit_event.set()
 
 signal.signal(signal.SIGINT, handler)
 
-def start_client():
-    global server_addr
-    global server_port
+def start_server():
     global server_sock
-    global sock
+    global client_sock
     global exit_event
-    global message_queue
-    global output
-    global dq_lock
-    global output_lock
+
     server_sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-    server_sock.bind((server_addr, server_port))
+    server_sock.bind(("", server_port))
     server_sock.listen(1)
-    server_sock.settimeout(10)
-    sock, address = server_sock.accept()
-    print("Connected")
-    server_sock.settimeout(None)
-    sock.setblocking(0)
+    print("Waiting for connection...")
+
+    client_sock, address = server_sock.accept()
+    print(f"Accepted connection from {address}")
+
     while not exit_event.is_set():
-        if dq_lock.acquire(blocking=False):
-            if(len(message_queue) > 0):
-                try:
-                    sent = sock.send(bytes(message_queue[0], 'utf-8'))
-                except Exception as e:
-                    exit_event.set()
-                    continue
-                if sent < len(message_queue[0]):
-                    message_queue[0] = message_queue[0][sent:]
-                else:
-                    message_queue.popleft()
-            dq_lock.release()
-        
-        if output_lock.acquire(blocking=False):
-            data = ""
-            try:
-                try:
-                    data = sock.recv(1024).decode('utf-8')
-                except socket.error as e:
-                    assert(1==1)
-                    #no data
+        try:
+            data = client_sock.recv(buf_size)
+            if not data:
+                break
+            print(f"Received: {data.decode('utf-8')}")
+            
+            # Echo the received data back to the client
+            client_sock.send(data)
+        except socket.error:
+            break
 
-            except Exception as e:
-                exit_event.set()
-                continue
-            output += data
-            output_split = output.split("\r\n")
-            for i in range(len(output_split) - 1):
-                print(output_split[i])
-            output = output_split[-1]
-            output_lock.release()
+    print("Disconnected")
+
+server_thread = threading.Thread(target=start_server)
+server_thread.start()
+
+try:
+    while not exit_event.is_set():
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("Interrupted by user")
+
+exit_event.set()
+if client_sock:
+    client_sock.close()
+if server_sock:
     server_sock.close()
-    sock.close()
-    print("client thread end")
-
-
-cth = threading.Thread(target=start_client)
-
-cth.start()
-
-j = 0
-while not exit_event.is_set():
-    dq_lock.acquire()
-    message_queue.append("RPi " + str(j) + " \r\n")
-    dq_lock.release()
-    j += 1
-    time.sleep(1.5)
-    
-
-print("Disconnected.")
-
-
 print("All done.")
--- windows_socket.py
+++ windows_socket.py
@@ -1,97 +1,27 @@
 import socket
-import threading
-from collections import deque
-import signal
 import time
 
-server_addr = 'D8:3A:DD:F8:70:E3'
+server_addr = 'D8:3A:DD:F8:70:E3'  # Make sure this is your Raspberry Pi's Bluetooth address
 server_port = 1
-
 buf_size = 1024
 
-client_sock = None
-server_sock = None
-sock = None
+sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+sock.connect((server_addr, server_port))
+print("Connected to the Raspberry Pi")
 
-exit_event = threading.Event()
-
-message_queue = deque([])
-output = ""
-
-dq_lock = threading.Lock()
-output_lock = threading.Lock()
-
-def handler(signum, frame):
-    exit_event.set()
-
-signal.signal(signal.SIGINT, handler)
-
-def start_client():
-    global sock
-    global dq_lock
-    global output_lock
-    global exit_event
-    global message_queue
-    global output
-    global server_addr
-    global server_port
-    sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-    sock.settimeout(10)
-    sock.connect((server_addr,server_port))
-    sock.settimeout(None)
-    print("after connect")
-    sock.setblocking(False)
-    while not exit_event.is_set():
-        if dq_lock.acquire(blocking=False):
-            if(len(message_queue) > 0):
-                try:
-                    sent = sock.send(bytes(message_queue[0], 'utf-8'))
-                except Exception as e:
-                    exit_event.set()
-                    continue
-                if sent < len(message_queue[0]):
-                    message_queue[0] = message_queue[0][sent:]
-                else:
-                    message_queue.popleft()
-            dq_lock.release()
+try:
+    for i in range(10):  # Send 10 messages
+        message = f"PC {i}\r\n"
+        sock.send(message.encode('utf-8'))
+        print(f"Sent: {message.strip()}")
         
-        if output_lock.acquire(blocking=False):
-            data = ""
-            try:
-                try:
-                    data = sock.recv(1024).decode("utf-8")
-                except socket.error as e:
-                    assert(1==1)
-                    #no data
-            except Exception as e:
-                exit_event.set()
-                continue
-            output += data
-            output_split = output.split("\r\n")
-            for i in range(len(output_split) - 1):
-                print(output_split[i])
-            output = output_split[-1]
-            output_lock.release()
+        data = sock.recv(buf_size)
+        print(f"Received: {data.decode('utf-8').strip()}")
+        
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("Interrupted by user")
+finally:
     sock.close()
-    print("client thread end")
-
-
-cth = threading.Thread(target=start_client)
-
-cth.start()
-
-
-print("finish join")
-j = 0
-while not exit_event.is_set():
-    dq_lock.acquire()
-    message_queue.append("PC " + str(j) + " \r\n")
-    dq_lock.release()
-    j += 1
-    time.sleep(2)
-
-print("Disconnected.")
-
-
-
-print("All done.")
+    print("Disconnected")
+    print("All done.")
